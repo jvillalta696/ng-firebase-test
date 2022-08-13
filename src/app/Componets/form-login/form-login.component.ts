ba8c375c705719ec818e80a00f197aa68ee5b468
jvillalta696 <jvillalta696@gmail.com>
2022-08-13 10:28:15 -0600

    Create user inteface(Worked)

--- src/app/Componets/form-login/form-login.component.ts
+++ src/app/Componets/form-login/form-login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { User } from 'src/app/Interfaces/user';
 
 @Component({
   selector: 'app-form-login',
@@ -9,18 +10,20 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormLoginComponent implements OnInit {
   //atributes
   loginForm:FormGroup;
-
+  user:User;
   constructor() { 
     this.loginForm = new FormGroup({
       email:new FormControl(''),
       psw:new FormControl('')
-    })
+    }),
+    this.user ={email:'',psw:''}
   };
 
   ngOnInit(): void {
   }
   submit(){
-    console.log(this.loginForm.value)
+    this.user = this.loginForm.value;
+    console.log(this.user)
   }
 
 }
