4e189800aa334f352e6480f36e7084a458f85a1b
jvillalta696 <jvillalta696@gmail.com>
2022-08-13 09:42:30 -0600

    Create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardViewComponent } from './Views/dashboard-view/dashboard-view.component';
+import { LoginViewComponent } from './Views/login-view/login-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login',
+component:LoginViewComponent},
+{
+  path:'',
+  component:DashboardViewComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
